3fed79cd66b610f823318d1334288796ec2de464
Dominik Zarsky <dominik.zarsky@gmail.com>
2025-12-14 11:41:04 +0100

    Extract SVG icons into reusable SolidJS components

---
+++ src/components/icons/index.ts
@@ -0,0 +1,4 @@
+export { GithubIcon } from './GithubIcon';
+export { LinkedinIcon } from './LinkedinIcon';
+export { EmailIcon } from './EmailIcon';
+export { ExternalLinkIcon } from './ExternalLinkIcon';
